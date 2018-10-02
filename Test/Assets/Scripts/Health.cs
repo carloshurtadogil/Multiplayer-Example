097d5ac6472f8bb48537b6e049539ba195644d3d
Carlos Hurtado <carlos.hurtado19@yahoo.com>
2018-10-01 17:46:28 -0700

    Current Version

---
+++ Test/Assets/Scripts/Health.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Health : NetworkBehaviour {
+
+    public const int maxHealth = 100;
+    //Whenever the health value changes on the server, the client will also be informed
+    [SyncVar (hook = "OnChangeHealth")] public int currentHealth = maxHealth;
+    public RectTransform healthbar;
+
+	public void TakeDamage(int amount) 
+    {
+        if(!isServer) 
+        {
+            return;
+        }
+
+        currentHealth -= amount;
+        if(currentHealth <= 0) {
+            currentHealth = 0;
+            Destroy(this.gameObject);
+            Debug.Log("Dead");
+        }
+
+    }
+
+    private void OnChangeHealth(int health)
+    {
+        healthbar.sizeDelta = new Vector2(health * 2, healthbar.sizeDelta.y);
+    }
+}
---
+++ Test/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+    public GameObject bulletObject;
+    public Transform bulletSpawn;
+    public float bulletSpeed;
+
+    void Update()
+    {
+        if(!isLocalPlayer) {
+            return;
+        }
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+        if(Input.GetKeyDown(KeyCode.Space)) 
+        {
+            CmdFire();
+        }
+    }
+
+    // Only local player can run the command in the server
+    [Command]
+    void CmdFire() 
+    {
+        //Create bullet based on the object provided
+        GameObject bullet = (GameObject)Instantiate(bulletObject, bulletSpawn.position, bulletSpawn.rotation);
+
+        //Add velocity to bullet object
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
+
+        //Spawn the bullet on the Clients
+        NetworkServer.Spawn(bullet);
+
+        //Destroy bullet after 2 seconds
+        Destroy(bullet, 2);
+    }
+
+	public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+
+}
